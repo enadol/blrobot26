5be54d5458d17d97641fae05a1e1399d27d31ee7
enadol <enadol@yahoo.com>
2026-01-28 17:23:54 +0100

    modified:   play26new.py

--- play26new.py
+++ play26new.py
@@ -197,7 +197,7 @@ def actualizar_open(TORNEO, lst_match, lst_jornadas, dates_final):
         # file.write("\ufeff") 
         # lista para manejar discrepancias en el número de partidos por jornada
         # CORREGIR SEGÚN TORNEO
-        line_exceptions=[0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99, 108, 117, 126, 135, 142, 150, 159]
+        line_exceptions=[0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99, 108, 117, 126, 135, 144, 153, 162]
         count_jornadas = 0
         for g, line in enumerate(lst_match):
             # En lugar de usar lst_match.index(line), es más eficiente usar enumerate(lst_match)
